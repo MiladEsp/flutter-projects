5e41d19893a84e26b8b4ad9a282fbbe0d098880e
MiladEsp <esmaeilpour.milad@gmail.com>
2021-04-05 17:47:30 -0400

    displaying recipe items

--- meals_app/lib/screens/category_meals_screen.dart
+++ meals_app/lib/screens/category_meals_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-import './dummy_data.dart';
+import '../dummy_data.dart';
+import '../widgets/meal_item.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
   // final String categoryId;
@@ -27,7 +28,13 @@ class CategoryMealsScreen extends StatelessWidget {
       ),
       body: ListView.builder(
         itemBuilder: (ctx, index) {
-          return Text(categoryMeals[index].title);
+          return MealItem(
+            title: categoryMeals[index].title,
+            imageUrl: categoryMeals[index].imageUrl,
+            affordability: categoryMeals[index].affordability,
+            complexity: categoryMeals[index].complexity,
+            duration: categoryMeals[index].duration,
+          );
         },
         itemCount: categoryMeals.length,
       ),

8b915aca30e96598566d27f25935d9cd87575cfb
MiladEsp <esmaeilpour.milad@gmail.com>
2021-04-08 22:13:53 -0400

    added TabBar to the AppBar

--- meals_app/lib/main.dart
+++ meals_app/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import './screens/categories_screen.dart';
 import './screens/category_meals_screen.dart';
 import './screens/meal_detail_screen.dart';
+import './screens/tabs_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -32,9 +33,10 @@ class MyApp extends StatelessWidget {
       //home: CategoriesScreen(),
       initialRoute: '/', // this is the default
       routes: {
-        '/': (ctx) => CategoriesScreen(),
+        '/': (ctx) => TabsScreen(),
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
         MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
+        //TabsScreen.routeName: (ctx) => TabsScreen(),
       },
       // onGenerateRoute: (settings) {
       //   return MaterialPageRoute(
---
+++ meals_app/lib/screens/tabs_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/screens/favorites_screen.dart';
+
+import './categories_screen.dart';
+
+class TabsScreen extends StatefulWidget {
+  static const routeName = '/tabs-screen';
+
+  @override
+  _TabsScreenState createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      // TabBar & TabBarView needed for this to work
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Meals'),
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(
+                icon: Icon(Icons.category),
+                text: 'Categories',
+              ),
+              Tab(
+                icon: Icon(Icons.star),
+                text: 'Favorites',
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            CategoriesScreen(),
+            FavoritesScreen(),
+          ],
+        ),
+      ),
+    );
+  }
+}

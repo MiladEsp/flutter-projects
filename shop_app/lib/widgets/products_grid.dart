ea87c84d414c37bec7833877cf63dd5e444f793b
MiladEsp <esmaeilpour.milad@gmail.com>
2021-04-17 13:35:32 -0400

    added provider for product class
    notifies the product item of changes made to each product

--- shop_app/lib/widgets/products_grid.dart
+++ shop_app/lib/widgets/products_grid.dart
@@ -19,10 +19,10 @@ class ProductsGrid extends StatelessWidget {
         crossAxisSpacing: 10,
         mainAxisSpacing: 10,
       ),
-      itemBuilder: (ctx, i) => ProductItem(
-        products[i].id,
-        products[i].title,
-        products[i].imageUrl,
+      itemBuilder: (ctx, i) => ChangeNotifierProvider(
+        create: (c) => products[i],
+        child:
+            ProductItem(), // the nearest product is passed inside ProductItem
       ),
       itemCount: products.length,
     );
